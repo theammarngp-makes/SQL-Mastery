df60440dd8e7d032a0d00a22687a2bfcbb821b76
Mohammad Ammar <theammarngp@gmail.com>
2026-06-20 23:29:27 +0530

    initial data inserts for employees, departments, and locations

---
+++ 10_Schema/02_NSERT_DATA.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO employes VALUES
+(1, 'Ammar', 1, 3),
+(2, 'Riya', 2, 3),
+(3, 'Sahil', 1, NULL),
+(4, 'Priya', 3, 2),
+(5, 'Arjun', 2, 1);
+
+INSERT INTO departments VALUES
+(1, 'Data Analytics', 1),
+(2, 'Engineering', 2),
+(3, 'Marketing', 1);
+
+INSERT INTO locations VALUES
+(1, 'Nagpur', 'India'),
+(2, 'Pune', 'India');
